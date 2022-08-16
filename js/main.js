574c05aa5893efc87e13f7bc7e818fbe03e71efe
• Hannah • <79538333+anner-H@users.noreply.github.com>
2022-08-16 12:07:38 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+let frutas = ["🥭", "🍍", "🍇", "🍓", "🍒", "🍎", "🍉", "🥝", "🍌", "🥭", "🍍", "🍇", "🍓", "🍒", "🍎", "🍉", "🥝", "🍌"];
+
+
+let root = document.querySelector("#root");
+
+//FUNCIONES
+
+function agregar_al_DOM(){
+
+    frutas.forEach(function (elemento, indice){
+
+        let contenedor = document.createElement("div");
+
+    contenedor.innerHTML = 
+    '<div class="fruta">' + 
+    elemento + 
+    '</div>';
+
+
+    root.appendChild(contenedor); 
+        
+        });
+
+    //ford
+
+}
+
+agregar_al_DOM();
+
+//ACTIVACIÓN DE LETRAS
+
+let datos_del_don = document.querySelectorAll(".fruta");
+
+function activar(){
+    this.classList.add("activo");
+}
+
+datos_del_don.forEach(function(elemento, indice){
+    
+    elemento.addEventListener('click', activar )
+});
